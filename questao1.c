1bf491bd4ad1975c3591e78e03c5751fdbb12892
gfreitazz <146898532+gfreitazz@users.noreply.github.com>
2023-11-29 21:56:24 -0300

    Add files via upload

---
+++ questao1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	
+	int numero1, numero2;
+	
+
+	printf("digite o numero 1:");
+	scanf("%d" , &numero1);
+	
+	printf("digite o numero 2:");
+	scanf("%d" , &numero2);
+}
+	if (multiplique 1 == 1){
+		printf("O resultado eh: %d \n" , numero1 * numero2);
+	}
+	 else if(numero2 == 0){
+	  printf("Nao eh possivel fazer divisao por zero");
+	}
+	 else if (opcao == 2){
+		printf("O resultado eh: %d \n" , numero1 % numero2);
+		
+	} else if(numero2 == 0){
+	  printf("Nao eh possivel fazer divisao por zero");}
+	
+return(0);
+	
+}
+
